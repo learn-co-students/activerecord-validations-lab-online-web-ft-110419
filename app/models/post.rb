8e33353c70cb6ba79e908a76ea872b6200e106b9
jessesbyers <jesse.s.byers@gmail.com>
2020-01-17 12:06:06 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 100}
+    validates :category, inclusion: {in: %w(small medium large Fiction)}
+    validate :title_clickbaity
+
+    def title_clickbaity
+       if title.present? && !title.match?(/...Won't Believe.../)
+            errors.add(:title, "title is invalid")
+        end
+    end
 end
+
