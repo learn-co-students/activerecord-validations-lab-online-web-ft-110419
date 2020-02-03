8ea2b122e95f9f8e8159c237d704aa3f693c1e43
nkizito92 <nkizito57@gmail.com>
2020-02-03 15:59:25 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} This is neither a Fiction or Non-Fiction category"}
+    validate :clickbait
+
+
+    def clickbait
+        if title == nil
+            errors.add(:title, "This is not clickbait")
+        else
+            if !title.match(/Won\'t Believe|Secret|Top [0-9]|Guess/)
+                errors.add(:title, "This is not clickbait")
+            end 
+        end
+    end 
 end
+# "Won't Believe", "Secret", "Top [number]", or "Guess"
