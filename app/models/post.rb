9c45fae6efa7b686a5a3e3d852259c91f881088c
Ivana <ivana_pile@yahoo.com>
2020-02-04 10:57:16 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,5 +11,5 @@ class Post < ActiveRecord::Base
         if !title.nil? && (!title.include?("You Won't" || "Secret" || "Top" || "Guess")) 
           errors.add(:clickbait, "That is not a clickbait worthy title. Try again.")
         end
-      end
+    end
 end
